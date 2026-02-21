ff62c866814a21003c3fa8ae1b4e188c51d4b3b2
kassemdaab-sketch <kassemdaab@gmail.com>
2026-02-21 10:04:20 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -2,16 +2,23 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
     const target = req.query.url;
-    if (!target) return res.status(400).send('Target URL required.');
+    
+    if (!target) {
+        return res.status(400).send('Error: No target URL provided.');
+    }
 
     return createProxyMiddleware({
         target: target,
         changeOrigin: true,
         followRedirects: true,
+        // This logic removes the "Black Screen" security headers
         onProxyRes: (proxyRes) => {
             delete proxyRes.headers['x-frame-options'];
             delete proxyRes.headers['content-security-policy'];
             delete proxyRes.headers['frame-options'];
+        },
+        onError: (err, req, res) => {
+            res.status(500).send('Proxy Error: Galaxy could not reach this site.');
         }
     })(req, res);
 };
